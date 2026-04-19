e90d334d97f616d7ef7fce8f964e996d43414374
ST10401917 <st10401917@vcconnect.edu.za>
2026-04-19 12:30:27 +0200

    Updated Service Request Class Model

---
+++ PROG7311 PART 2/Migrations/20260419102908_updatedServiceModelClass.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PROG7311_PART_2.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedServiceModelClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "ServiceRequests",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "ServiceRequests",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+    }
+}
--- PROG7311 PART 2/Models/ServiceRequest.cs
+++ PROG7311 PART 2/Models/ServiceRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace PROG7311_PART_2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PROG7311_PART_2.Models
 {
     public enum RequestStatus
     {
@@ -9,13 +11,20 @@
 
     public class ServiceRequest
     {
+        [Key]
         public int ServiceRequestId { get; set; }
 
+        [Required(ErrorMessage = "Contract is required")]
         public int ContractId { get; set; }
-        public Contract Contract { get; set; }
 
-        public string Description { get; set; }
+        public Contract? Contract { get; set; }
 
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(200)]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "USD cost is required")]
+        [Range(0.01, 1000000, ErrorMessage = "Cost must be greater than 0")]
         public decimal CostUSD { get; set; }
         public decimal CostZAR { get; set; }
 
