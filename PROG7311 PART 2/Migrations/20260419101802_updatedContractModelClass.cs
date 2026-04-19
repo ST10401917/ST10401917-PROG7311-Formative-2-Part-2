816ad8959f518688d95f0036f479088ceb733415
ST10401917 <st10401917@vcconnect.edu.za>
2026-04-19 12:20:16 +0200

    Updated Client and Contract Model class

---
+++ PROG7311 PART 2/Migrations/20260419101802_updatedContractModelClass.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PROG7311_PART_2.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedContractModelClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ServiceLevel",
+                table: "Contracts",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ServiceLevel",
+                table: "Contracts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
--- PROG7311 PART 2/Models/Contract.cs
+++ PROG7311 PART 2/Models/Contract.cs
@@ -1,4 +1,6 @@
-﻿namespace PROG7311_PART_2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PROG7311_PART_2.Models
 {
     public enum ContractStatus
     {
@@ -9,18 +11,31 @@
     }
     public class Contract
     {
+        [Key]
         public int ContractId { get; set; }
 
+        [Required(ErrorMessage = "Client is required")]
         public int ClientId { get; set; }
 
         public Client? Client { get; set; }
 
+        [Required(ErrorMessage = "Start date is required")]
+        [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "End date is required")]
+        [DataType(DataType.Date)]
         public DateTime EndDate { get; set; }
 
+        [Required(ErrorMessage = "Status is required")]
         public ContractStatus Status { get; set; }
+
+        [Required(ErrorMessage = "Service level is required")]
+        [StringLength(50)]
         public string? ServiceLevel { get; set; }
 
+        // Optional file (but you can make it required if needed)
+        [Display(Name = "Signed Agreement")]
         public string? SignedAgreementPath { get; set; }
 
         public ICollection<ServiceRequest> ServiceRequests { get; set; } = new List<ServiceRequest>();
